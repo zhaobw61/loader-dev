7efdc5c7e70fc21f879015b4b5095a17e2ebbcaa
zhaobw <1964471757@qq.com>
2019-07-21 16:23:05 +0800

    初始化loader

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # loader-dev
 编写和学习loader
+
+### loader概念
+
+- webpack只能处理JS模块，如果要处理其他类型的文件，需要用loader进行转换。loader是webpack中一个概念。它是用来讲一段代码转换成另一段代码的webpack加载器。
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path');
+
+module.exports = {
+    mode:'development',
+    entry:'./src/index.js',
+    output:{
+        filename:'build.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    resolveLoader:{
+        modules:['node_modules',path.resolve(__dirname,'loaders')]
+        // alias:{
+        //     loader1:path.reslove(__dirname,'loaders','loader1.js')
+        // }
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                use:'loader1'
+            }
+        ]
+    }
+}
